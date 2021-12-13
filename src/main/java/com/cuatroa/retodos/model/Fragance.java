b717b2b6e2aa0f2197a7a9eab4ce62baa7588e48
juand2323 <juandiegojds23@gmail.com>
2021-12-12 21:14:39 -0500

    back reto3.4

--- src/main/java/com/cuatroa/retodos/model/Fragance.java
+++ src/main/java/com/cuatroa/retodos/model/Fragance.java
@@ -19,7 +19,7 @@ public class Fragance {
     private String reference;
     private String brand;
     private String category;
-    private String presentation;
+    private String objetivo;
     private String description;
     private boolean availability = true;
     private double price;
